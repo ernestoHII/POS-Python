0e82f582c2764f7e0f28f9045f0b902291b1f62e
ernestoHII <ernestoalmedajr@hiro-c.net>
2023-09-27 16:22:58 +0800

    List Item loading items from DB preview no added features yet

---
+++ Item List.py
@@ -0,0 +1,86 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QTableWidget, QTableWidgetItem, QLabel
+import pyodbc
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyQt5 Table from MS SQL Server")
+        self.setGeometry(100, 100, 1366, 768)  # Set the window size to 1366x768 pixels
+
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        grid_layout = QGridLayout(central_widget)
+
+        # Create a QLabel to display connection status
+        self.connection_status_label = QLabel()
+        grid_layout.addWidget(self.connection_status_label, 0, 0, 1, 2)
+
+        # Create a QTableWidget with columns for specific data
+        self.table_widget = QTableWidget()
+        headers = ["Item Code", "Barcode", "Item Description", "Unit", "Category", "Default Supplier", "Price", "Quantity", "Inventory", "Locked"]
+        self.table_widget.setColumnCount(len(headers))
+        self.table_widget.setHorizontalHeaderLabels(headers)
+
+        # Attempt to fetch data from MS SQL Server and populate the table
+        if self.fetch_data_and_populate_table():
+            self.connection_status_label.setText("Connected to database")
+        else:
+            self.connection_status_label.setText("Failed to connect to database")
+
+        # Add the QTableWidget to the layout
+        grid_layout.addWidget(self.table_widget, 1, 0, 1, 2)
+
+    def fetch_data_and_populate_table(self):
+        try:
+            # Define the MS SQL Server connection string
+            server = 'DESKTOP-JDQGAO5'
+            database = 'easypos'  # Change to your database name
+            username = 'sa'
+            password = 'easyfis'
+            connection_string = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+            # Establish a connection
+            conn = pyodbc.connect(connection_string)
+
+            # Create a cursor
+            cursor = conn.cursor()
+
+            # Execute SQL query to fetch specific columns from MstItem table
+            query = '''
+            SELECT MI.ItemCode, MI.Barcode, MI.ItemDescription, MU.Unit AS Unit, MI.Category, MS.Supplier AS Supplier, MI.Price, MI.OnhandQuantity, MI.IsInventory, MI.IsLocked
+            FROM MstItem AS MI
+            LEFT JOIN MstUnit AS MU ON MI.UnitId = MU.Id
+            LEFT JOIN MstSupplier AS MS ON MI.DefaultSupplierId = MS.Id
+            ORDER BY MI.ItemDescription ASC  -- Order by ItemDescription in ascending order (alphabetical)
+            
+            '''
+
+            cursor.execute(query)
+
+            # Fetch all rows from the query result
+            rows = cursor.fetchall()
+
+            # Populate the QTableWidget with data
+            self.table_widget.setRowCount(len(rows))
+            for row_num, row_data in enumerate(rows):
+                for col_num, cell_value in enumerate(row_data):
+                    item = QTableWidgetItem(str(cell_value))
+                    self.table_widget.setItem(row_num, col_num, item)
+
+            # Close the cursor and connection
+            cursor.close()
+            conn.close()
+
+            return True
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            return False
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
--- dropdown.py
+++ dropdown.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QAction, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QAction, QLabel, QVBoxLayout, QWidget, QPushButton
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -14,17 +14,26 @@ class MyWindow(QMainWindow):
         label = QLabel("Welcome to Setup Menu", self)
         layout.addWidget(label)
 
-        setup_menu = self.menuBar().addMenu("Setup")
+        setup_menu = QMenu("Setup", self)
 
-        # Create submenu items
+        # Create submenu items (unchanged)
         item_action = QAction("Item", self)
         item_action.triggered.connect(self.item_action_triggered)
 
         customer_action = QAction("Customer", self)
         customer_action.triggered.connect(self.customer_action_triggered)
 
-        discount_action = QAction("Discount", self)
-        discount_action.triggered.connect(self.discount_action_triggered)
+        # Create a submenu for "Discount" and add items to it (unchanged)
+        discount_submenu = QMenu("Discount", self)
+
+        fixed_discount_action = QAction("Fixed Discount", self)
+        fixed_discount_action.triggered.connect(self.fixed_discount_action_triggered)
+
+        percentage_discount_action = QAction("Percentage Discount", self)
+        percentage_discount_action.triggered.connect(self.percentage_discount_action_triggered)
+
+        discount_submenu.addAction(fixed_discount_action)
+        discount_submenu.addAction(percentage_discount_action)
 
         currency_action = QAction("Currency", self)
         currency_action.triggered.connect(self.currency_action_triggered)
@@ -32,12 +41,25 @@ class MyWindow(QMainWindow):
         user_action = QAction("User", self)
         user_action.triggered.connect(self.user_action_triggered)
 
-        # Add submenu items to the "Setup" menu
+        # Add submenu items to the "Setup" menu (unchanged)
         setup_menu.addAction(item_action)
         setup_menu.addAction(customer_action)
-        setup_menu.addAction(discount_action)
+        
+        # Add the "Discount" submenu to the "Setup" menu (unchanged)
+        setup_menu.addMenu(discount_submenu)
+
         setup_menu.addAction(currency_action)
         setup_menu.addAction(user_action)
+
+        # Set the menu bar to display the "Setup" menu
+        self.menuBar().addMenu(setup_menu)
+
+        # Create a button to toggle the menu
+        self.toggle_menu_button = QPushButton("Toggle Menu", self)
+        self.toggle_menu_button.clicked.connect(self.toggle_menu)
+        layout.addWidget(self.toggle_menu_button)
+
+        self.menu_visible = True
 
     def item_action_triggered(self):
         print("Item menu item clicked.")
@@ -45,14 +67,25 @@ class MyWindow(QMainWindow):
     def customer_action_triggered(self):
         print("Customer menu item clicked.")
 
-    def discount_action_triggered(self):
-        print("Discount menu item clicked.")
+    def fixed_discount_action_triggered(self):
+        print("Fixed Discount menu item clicked.")
+
+    def percentage_discount_action_triggered(self):
+        print("Percentage Discount menu item clicked.")
 
     def currency_action_triggered(self):
         print("Currency menu item clicked.")
 
     def user_action_triggered(self):
         print("User menu item clicked.")
+
+    def toggle_menu(self):
+        if self.menu_visible:
+            self.menuBar().setVisible(False)
+            self.menu_visible = False
+        else:
+            self.menuBar().setVisible(True)
+            self.menu_visible = True
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
